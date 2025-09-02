1956bac00fc18cb1d06187e40f4229b1a55f8d80
samolego <34912839+samolego@users.noreply.github.com>
2025-09-02 12:00:29 +0200

    remove svgs

--- web-app-pare/src/ui/components/panels/forms/UserSelectedContainer.vue
+++ web-app-pare/src/ui/components/panels/forms/UserSelectedContainer.vue
@@ -8,13 +8,6 @@
         class="avatar-image"
         @error="onAvatarError"
       />
-      <span v-else class="avatar-fallback oc-icon oc-icon-s">
-        <svg viewBox="0 0 24 24" fill="currentColor">
-          <path
-            d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
-          />
-        </svg>
-      </span>
     </div>
     <div class="user-info">
       <div class="user-name" v-text="user.displayName" />
